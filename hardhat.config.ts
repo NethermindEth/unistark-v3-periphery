17cf1ee8d4102f09a82157f6b8d3adcc73a59405
rodrigo-pino <rodrodpino@gmail.com>
2022-11-08 07:43:57 -0500

    Make tests transpilable

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,14 +3,14 @@ import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-waffle'
 import 'hardhat-typechain'
 import 'hardhat-watcher'
+import 'hardhat-warp'
 
-const LOW_OPTIMIZER_COMPILER_SETTINGS = {
-  version: '0.7.6',
+const WARP_COMPILER_SETTINGS = {
+  version: '0.8.14',
   settings: {
-    evmVersion: 'istanbul',
     optimizer: {
       enabled: true,
-      runs: 2_000,
+      runs: 800,
     },
     metadata: {
       bytecodeHash: 'none',
@@ -18,36 +18,26 @@ const LOW_OPTIMIZER_COMPILER_SETTINGS = {
   },
 }
 
-const LOWEST_OPTIMIZER_COMPILER_SETTINGS = {
-  version: '0.7.6',
-  settings: {
-    evmVersion: 'istanbul',
-    optimizer: {
-      enabled: true,
-      runs: 1_000,
-    },
-    metadata: {
-      bytecodeHash: 'none',
-    },
-  },
-}
-
-const DEFAULT_COMPILER_SETTINGS = {
-  version: '0.7.6',
-  settings: {
-    evmVersion: 'istanbul',
-    optimizer: {
-      enabled: true,
-      runs: 1_000_000,
-    },
-    metadata: {
-      bytecodeHash: 'none',
-    },
-  },
-}
+const port = (Math.floor(Math.random() * 1000) + 5000).toString()
 
 export default {
   networks: {
+    integratedDevnet: {
+      url: `http://127.0.0.1:${port}`,
+
+      venv: 'active', // <- for the active virtual environment with installed starknet-devnet
+      // venv: "path/to/venv" <- for env with installed starknet-devnet (created with e.g. `python -m venv path/to/venv`)
+      // venv: "../../starknet-devnet/.env",
+
+      // optional devnet CLI arguments
+      args: ['--seed', '0', '--timeout', '10000'],
+
+      // stdout: "logs/stdout.log" <- dumps stdout to the file
+      stdout: `.${port}.log`, // <- logs stdout to the terminal
+      // stderr: "logs/stderr.log" <- dumps stderr to the file
+      stderr: 'STDERR', // <- logs stderr to the terminal
+    },
+
     hardhat: {
       allowUnlimitedContractSize: false,
     },
@@ -84,16 +74,7 @@ export default {
     // Obtain one at https://etherscan.io/
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
-  solidity: {
-    compilers: [DEFAULT_COMPILER_SETTINGS],
-    overrides: {
-      'contracts/NonfungiblePositionManager.sol': LOW_OPTIMIZER_COMPILER_SETTINGS,
-      'contracts/test/MockTimeNonfungiblePositionManager.sol': LOW_OPTIMIZER_COMPILER_SETTINGS,
-      'contracts/test/NFTDescriptorTest.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
-      'contracts/NonfungibleTokenPositionDescriptor.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
-      'contracts/libraries/NFTDescriptor.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
-    },
-  },
+  solidity: WARP_COMPILER_SETTINGS,
   watcher: {
     test: {
       tasks: [{ command: 'test', params: { testFiles: ['{path}'] } }],
