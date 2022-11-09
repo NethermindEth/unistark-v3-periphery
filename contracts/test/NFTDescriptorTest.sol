e5ddde48c4ef8622345863468035c5e0ea7a5ca9
Luis Lara <62342483+LuisLara-UH@users.noreply.github.com>
2022-11-08 22:05:01 -0500

    Remove structs on NFTDescriptor

--- contracts/test/NFTDescriptorTest.sol
+++ contracts/test/NFTDescriptorTest.sol
@@ -9,21 +9,78 @@ import '../libraries/HexStrings.sol';
 contract NFTDescriptorTest {
     using HexStrings for uint256;
 
-    function constructTokenURI(NFTDescriptor.ConstructTokenURIParams calldata params)
+    function constructTokenURI(
+        uint256 tokenId,
+        address quoteTokenAddress,
+        address baseTokenAddress,
+        string calldata quoteTokenSymbol,
+        string calldata baseTokenSymbol,
+        uint8 quoteTokenDecimals,
+        uint8 baseTokenDecimals,
+        bool flipRatio,
+        int24 tickLower,
+        int24 tickUpper,
+        int24 tickCurrent,
+        int24 tickSpacing,
+        uint24 fee,
+        address poolAddress)
         public
         pure
         returns (string memory)
     {
-        return NFTDescriptor.constructTokenURI(params);
+        return NFTDescriptor.constructTokenURI(
+            tokenId,
+            quoteTokenAddress,
+            baseTokenAddress,
+            quoteTokenSymbol,
+            baseTokenSymbol,
+            quoteTokenDecimals,
+            baseTokenDecimals,
+            flipRatio,
+            tickLower,
+            tickUpper,
+            tickCurrent,
+            tickSpacing,
+            fee,
+            poolAddress);
     }
 
-    function getGasCostOfConstructTokenURI(NFTDescriptor.ConstructTokenURIParams calldata params)
+    function getGasCostOfConstructTokenURI(
+        uint256 tokenId,
+        address quoteTokenAddress,
+        address baseTokenAddress,
+        string calldata quoteTokenSymbol,
+        string calldata baseTokenSymbol,
+        uint8 quoteTokenDecimals,
+        uint8 baseTokenDecimals,
+        bool flipRatio,
+        int24 tickLower,
+        int24 tickUpper,
+        int24 tickCurrent,
+        int24 tickSpacing,
+        uint24 fee,
+        address poolAddress
+    )
         public
         view
         returns (uint256)
     {
         uint256 gasBefore = gasleft();
-        NFTDescriptor.constructTokenURI(params);
+        NFTDescriptor.constructTokenURI(
+            tokenId,
+            quoteTokenAddress,
+            baseTokenAddress,
+            quoteTokenSymbol,
+            baseTokenSymbol,
+            quoteTokenDecimals,
+            baseTokenDecimals,
+            flipRatio,
+            tickLower,
+            tickUpper,
+            tickCurrent,
+            tickSpacing,
+            fee,
+            poolAddress);
         return gasBefore - gasleft();
     }
 
@@ -53,8 +110,37 @@ contract NFTDescriptorTest {
         return NFTDescriptor.addressToString(_address);
     }
 
-    function generateSVGImage(NFTDescriptor.ConstructTokenURIParams memory params) public pure returns (string memory) {
-        return NFTDescriptor.generateSVGImage(params);
+    function generateSVGImage(
+        uint256 tokenId,
+        address quoteTokenAddress,
+        address baseTokenAddress,
+        string calldata quoteTokenSymbol,
+        string calldata baseTokenSymbol,
+        uint8 quoteTokenDecimals,
+        uint8 baseTokenDecimals,
+        bool flipRatio,
+        int24 tickLower,
+        int24 tickUpper,
+        int24 tickCurrent,
+        int24 tickSpacing,
+        uint24 fee,
+        address poolAddress
+        ) public pure returns (string memory) {
+        return NFTDescriptor.generateSVGImage(
+            tokenId,
+            quoteTokenAddress,
+            baseTokenAddress,
+            quoteTokenSymbol,
+            baseTokenSymbol,
+            quoteTokenDecimals,
+            baseTokenDecimals,
+            flipRatio,
+            tickLower,
+            tickUpper,
+            tickCurrent,
+            tickSpacing,
+            fee,
+            poolAddress);
     }
 
     function tokenToColorHex(address token, uint256 offset) public pure returns (string memory) {
